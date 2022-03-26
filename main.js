641605ede5e0a55270a6b6289a8e09953ec6b263
reza rastegar <85369490+rezarastegar2003@users.noreply.github.com>
2022-03-26 18:12:41 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const toggler = document.getElementById("toggle");
+const sidebar = document.getElementById("sidebar");
+toggle.addEventListener("click", (e) => {
+  toggle.classList.toggle("active");
+  sidebar.classList.toggle("active");
+});
